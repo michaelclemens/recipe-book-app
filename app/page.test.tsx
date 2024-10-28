aa344710c815388aee3543c2c46e9dd0ca8a99ec
michaelclemens <michaelclemens8@hotmail.com>
2024-10-28 11:47:09 +0000

    Running Prettier

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react'
-import Home from './page'
 import { describe, expect, test } from 'vitest'
+import Home from './page'
 
 describe('HomeComponent', () => {
   test('Should render correctly', () => {
