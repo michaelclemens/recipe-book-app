cc3f0c392d868142a358ab2329818fb7530806eb
michaelclemens <michaelclemens8@hotmail.com>
2025-02-25 13:00:39 +0000

    Running prettier fix after tailwind css class name order changes

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -4,7 +4,7 @@ export default function Button(props: ButtonProps) {
   return (
     <UiButton
       {...props}
-      className={`${props.className} rounded-md bg-slate-900 px-3 py-2.5 text-slate-500 ring-1 shadow-sm ring-indigo-900 transition-colors duration-500 ring-inset enabled:hover:bg-slate-800 disabled:opacity-50 disabled:select-none`}
+      className={`${props.className} rounded-md bg-slate-900 px-3 py-2.5 text-slate-500 shadow-sm ring-1 ring-indigo-900 transition-colors duration-500 ring-inset enabled:hover:bg-slate-800 disabled:opacity-50 disabled:select-none`}
     >
       {props.children}
     </UiButton>
